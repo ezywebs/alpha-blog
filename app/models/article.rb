5aeaa6897413158ec3fcd90c18b45904fa6d0546
Alex <info@ezwebsupport.com>
2018-03-08 04:46:11 +0000

    Add associans users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+  validates :user_id, presence: true
   validates :title, length: {minimum: 3, maximum: 50}
   validates :description, length: {minimum: 10, maximum: 300}
   
